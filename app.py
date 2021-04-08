812c4fe678ef7473f6c14164ef427335874d5cc0
RainTwo <wangzhe92824@qq.com>
2021-04-08 14:47:09 +0800

    Add unit test with unittest

--- app.py
+++ app.py
@@ -154,9 +154,9 @@ def home():
     return 'Welcome to My Watchlist!'
 
 
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % escape(name)  # escape 防止输入的数据包含恶意代码 对name变量进行转义处理
+# @app.route('/user/<name>')
+# def user_page(name):
+#     return 'User: %s' % escape(name)  # escape 防止输入的数据包含恶意代码 对name变量进行转义处理
 
 
 # 编辑用户名
@@ -180,74 +180,74 @@ def settings():
     return render_template('settings.html')
 
 
-@app.route('/test')
-def test_url_for():
-    # print(url_for('hello'))
-    # print(url_for('user_page', name='greyli'))
-    # print(url_for('test_url_for'))
-    return 'Test page'
+# @app.route('/test')
+# def test_url_for():
+#     # print(url_for('hello'))
+#     # print(url_for('user_page', name='greyli'))
+#     # print(url_for('test_url_for'))
+#     return 'Test page'
 
 
 
-@app.cli.command()  # 注册为命令
-@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
-def initdb(drop):
-    """Initialize the database."""
-    if drop:  # 判断是否输入了选项
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.')  # 输出提示信息
+# @app.cli.command()  # 注册为命令
+# @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+# def initdb(drop):
+#     """Initialize the database."""
+#     if drop:  # 判断是否输入了选项
+#         db.drop_all()
+#     db.create_all()
+#     click.echo('Initialized database.')  # 输出提示信息
 
 
-@app.cli.command()
-@click.option('--username', prompt=True, help='The username used to login.')
-@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
-def admin(username, password):
-    """Create user."""
-    db.create_all()
+# @app.cli.command()
+# @click.option('--username', prompt=True, help='The username used to login.')
+# @click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+# def admin(username, password):
+#     """Create user."""
+#     db.create_all()
 
-    user = User.query.first()
-    if user is not None:
-        click.echo('Updating user...')
-        user.username = username
-        user.set_password(password)  # 设置密码
-    else:
-        click.echo('Creating user...')
-        user = User(username=username, name='Admin')
-        user.set_password(password)  # 设置密码
-        db.session.add(user)
+#     user = User.query.first()
+#     if user is not None:
+#         click.echo('Updating user...')
+#         user.username = username
+#         user.set_password(password)  # 设置密码
+#     else:
+#         click.echo('Creating user...')
+#         user = User(username=username, name='Admin')
+#         user.set_password(password)  # 设置密码
+#         db.session.add(user)
 
-    db.session.commit()  # 提交数据库会话
-    click.echo('Done.')
+#     db.session.commit()  # 提交数据库会话
+#     click.echo('Done.')
 
 
 
-@app.cli.command()
-def forge():
-    """Generate fake data."""
-    db.create_all()
+# @app.cli.command()
+# def forge():
+#     """Generate fake data."""
+#     db.create_all()
 
-    # 全局的两个变量移动到这个函数内
-    name = 'Grey Li'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
+#     # 全局的两个变量移动到这个函数内
+#     name = 'Grey Li'
+#     movies = [
+#         {'title': 'My Neighbor Totoro', 'year': '1988'},
+#         {'title': 'Dead Poets Society', 'year': '1989'},
+#         {'title': 'A Perfect World', 'year': '1993'},
+#         {'title': 'Leon', 'year': '1994'},
+#         {'title': 'Mahjong', 'year': '1996'},
+#         {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#         {'title': 'King of Comedy', 'year': '1999'},
+#         {'title': 'Devils on the Doorstep', 'year': '1999'},
+#         {'title': 'WALL-E', 'year': '2008'},
+#         {'title': 'The Pork of Music', 'year': '2012'},
+#     ]
 
-    user = User(name=name)
-    db.session.add(user)
-    for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])
-        db.session.add(movie)
+#     user = User(name=name)
+#     db.session.add(user)
+#     for m in movies:
+#         movie = Movie(title=m['title'], year=m['year'])
+#         db.session.add(movie)
 
-    db.session.commit()
-    click.echo('Done.')
+#     db.session.commit()
+#     click.echo('Done.')
 
